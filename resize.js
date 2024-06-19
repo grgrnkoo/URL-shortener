7da708df24635562070122b0c9b06f042516c876
Oleg <grigorenko996@gmail.com>
2024-06-19 20:06:10 +0300

    fully responsive website. all size adjustments

---
+++ resize.js
@@ -0,0 +1,29 @@
+const menuButton = document.querySelector('.menubutton');
+const currentWidth = window.innerWidth;
+const menu = document.querySelector('.menu');
+
+window.addEventListener('resize', (e) => {
+    menuMobileDesktop(e.target.innerWidth);
+});
+
+const menuMobileDesktop = (width) => {
+    width > 768 ? menu.classList.replace('mobile', 'desktop') : menu.classList.replace('desktop', 'mobile');
+
+    if (width < 432) {
+        menu.style.width = `${width - 32}px`;
+    }
+}
+
+menuMobileDesktop(currentWidth);
+
+const toggleMenu = () => {
+    menu.classList.toggle('hidden');
+}
+
+menuButton.addEventListener('click', toggleMenu);
+
+window.addEventListener('click', (e) => {
+    if (!menu.contains(e.target) && !menu.classList.contains('hidden') && !menuButton.contains(e.target)) {
+        menu.classList.add('hidden');
+    }
+});
